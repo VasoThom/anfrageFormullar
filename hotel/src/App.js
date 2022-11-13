a293932bf471b6c97b6ded10a6e8a328928a7e45
Vaso <vasothom@hotmail.com>
2022-11-13 11:47:57 +0100

    ..

---
+++ hotel/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Inputs from "./components/inputs";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Inputs />
+      </header>
+    </div>
+  );
+}
+
+export default App;
